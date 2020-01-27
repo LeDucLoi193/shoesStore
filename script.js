244d8f302401d43facb9f44b7d5e36806b4ede43
leducloi <dekaito193@gmail.com>
2020-01-27 17:59:46 +0700

    dropdown menu tam on, them cart, user icon

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
-  $('#loaiGiay').mouseenter(function(event){
-    event.stopPropagation();
-    $(this).find('ul').slideDown();
+  var inLoaiGiayUl = false;
+  var inLoaiGiay = false;
+  var appearUl = false;
+
+  $('#loaiGiay').mouseenter(function(){
+    inLoaiGiay = true;
+    if (appearUl == false){
+      $(this).find('ul').slideDown();
+    }
   });
-  $('#loaiGiay').mouseleave(function(event){
+  $('#loaiGiay > ul').mouseenter(function(event){
     event.stopPropagation();
+    inLoaiGiayUl = true;
+    inLoaiGiay = false;
+    appearUl = true;
+    console.log(inLoaiGiayUl, inLoaiGiay);
+  });
+  $('#loaiGiay > ul').mouseleave(function(event){
+    // event.stopPropagation();
+    console.log(inLoaiGiayUl, inLoaiGiay);
+    inLoaiGiayUl = false;
+    if (inLoaiGiay == false && inLoaiGiayUl == false){
+      $(this).slideUp();
+      appearUl = false;
+    }
+  });
+  $('#loaiGiay').mouseleave(function(){
     $(this).find('ul').slideUp();
   });
 
